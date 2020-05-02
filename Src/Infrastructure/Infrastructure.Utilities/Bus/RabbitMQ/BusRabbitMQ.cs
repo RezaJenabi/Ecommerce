2545d03dd5b9c21a480549c801c5fa011eab0b2d
pc2 <pc2@DESKTOP-0JP2S3D>
2020-05-02 17:08:46 +0430

    init bus rabbitMQ

---
+++ Src/Infrastructure/Infrastructure.Utilities/Bus/RabbitMQ/BusRabbitMQ.cs
@@ -0,0 +1,113 @@
+﻿using Infrastructure.Utilities.EventBus;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.Utilities.Extensions;
+
+namespace Infrastructure.Utilities.Bus.RabbitMQ
+{
+    public class BusRabbitMQ : IBus, IDisposable
+    {
+        const string BROKER_NAME = "ecommerce_bus";
+
+        private readonly IRabbitMQConnection _persistentConnection;
+        private readonly ILogger<BusRabbitMQ> _logger;
+        private readonly string AUTOFAC_SCOPE_NAME = "eshop_event_bus";
+        private readonly int _retryCount;
+
+        private IModel _consumerChannel;
+        private string _queueName;
+
+        public BusRabbitMQ(IRabbitMQConnection persistentConnection, ILogger<BusRabbitMQ> logger,
+            string queueName = null, int retryCount = 5)
+        {
+            _persistentConnection = persistentConnection ?? throw new ArgumentNullException(nameof(persistentConnection));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _queueName = queueName;
+            _consumerChannel = CreateConsumerChannel();
+            _retryCount = retryCount;
+        }
+
+
+
+        public void Dispose()
+        {
+            if (_consumerChannel != null)
+            {
+                _consumerChannel.Dispose();
+            }
+        }
+
+        public void Publish(IntegrationEvent @event)
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            _logger.LogTrace("Creating RabbitMQ consumer channel");
+
+            using (var channel = _persistentConnection.CreateModel())
+            {
+
+                _logger.LogTrace("Declaring RabbitMQ exchange to publish event: {EventId}", @event.Id);
+
+                channel.ExchangeDeclare(exchange: BROKER_NAME, type: "direct");
+
+                var eventName = @event.GetType().Name;
+
+                var message = @event.SerializeObject();
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.DeliveryMode = 2; // persistent
+
+                _logger.LogTrace("Publishing event to RabbitMQ: {EventId}", @event.Id);
+
+                channel.BasicPublish(
+                    exchange: BROKER_NAME,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body);
+            }
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            _logger.LogTrace("Creating RabbitMQ consumer channel");
+
+            var channel = _persistentConnection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: BROKER_NAME,
+                                    type: "direct");
+
+            channel.QueueDeclare(queue: _queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.CallbackException += (sender, ea) =>
+            {
+                _logger.LogWarning(ea.Exception, "Recreating RabbitMQ consumer channel");
+
+                _consumerChannel.Dispose();
+                _consumerChannel = CreateConsumerChannel();
+            };
+
+            return channel;
+        }
+
+
+    }
+}
